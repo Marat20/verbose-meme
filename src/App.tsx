8d60329905caadfd40e3a5f55424aeb2751be006
Marat Shakirov <unclemarat@gmail.com>
2023-11-17 19:33:46 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { Button } from './components/Button';
+import { Suspense } from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 export const App = () => {
   return (
-    <div className='app'>
-      <Button />
-    </div>
+    <BrowserRouter>
+      <div className='app'>
+        <Link to={'/'}>Главная</Link>
+        <Link to={'/about'}>О сайте</Link>
+        <Suspense fallback={<div>...Loading</div>}>
+          <Routes>
+            <Route path={'/about'} element={<AboutPageAsync />} />
+            <Route path={'/'} element={<MainPageAsync />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </BrowserRouter>
   );
 };
