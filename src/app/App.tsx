2b4248052cd1f79b8c600d8c2b1677670303be46
Marat Shakirov <unclemarat@gmail.com>
2023-11-19 11:55:35 +0300

    redesign app to feature-sliced design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,23 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 import { Suspense } from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { UseTheme } from './theme/UseTheme';
+import { UseTheme } from './providers/ThemeProvider/lib/UseTheme';
 
 export const App = () => {
   const { theme, toggleTheme } = UseTheme();
 
   return (
     <BrowserRouter>
-      <div className={`app ${theme}`}>
+      <div className={classNames('app', {}, [theme])}>
         <button onClick={toggleTheme}>TOGGLE</button>
         <Link to={'/'}>Главная</Link>
         <Link to={'/about'}>О сайте</Link>
         <Suspense fallback={<div>...Loading</div>}>
           <Routes>
-            <Route path={'/about'} element={<AboutPageAsync />} />
-            <Route path={'/'} element={<MainPageAsync />} />
+            <Route path={'/about'} element={<AboutPage />} />
+            <Route path={'/'} element={<MainPage />} />
           </Routes>
         </Suspense>
       </div>
