de03fde920852dbbbbc30899057a08b46aa94144
Marat Shakirov <unclemarat@gmail.com>
2023-11-27 21:04:22 +0300

    fix bugs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { Modal } from 'shared/ui/Modal';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { UseTheme } from './providers/ThemeProvider';
@@ -8,14 +7,11 @@ import { AppRouter } from './providers/router';
 
 export const App = () => {
   const { theme } = UseTheme();
-  const [isOpen, setIsOped] = useState(false);
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback=''>
         <Navbar />
-
-        <button onClick={() =>  setIsOped(true)}>tog</button>
         <div className='content-page'>
           <Sidebar />
           <AppRouter />
